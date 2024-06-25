661c7c18c99d0bb38b913d234ef69813d1355855
John <136768731+jpt1729@users.noreply.github.com>
2024-06-25 02:09:54 -0500

    workinggggg

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    library: 'my-react-library',
+    library: 'texan-spyware',
     libraryTarget: 'umd',
   },
   module: {
